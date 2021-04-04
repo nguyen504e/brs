4758e9f4cb3e83428ce1d43e2c100cf5cc4b02da
Nguyen Pham Nguyen <nguyen.ph.nguyen@gbst.com>
2021-04-04 23:27:45 +0700

    Initial commit

--- src/client/components/ContentEditor/index.jsx
+++ src/client/components/ContentEditor/index.jsx
@@ -1,52 +1,58 @@
-import React, { useRef, useState } from 'react'
-import Card from 'react-bootstrap/Card'
-import ToggleButton from 'react-bootstrap/ToggleButton'
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
-import FormControl from 'react-bootstrap/FormControl'
-import classNames from 'classnames'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-import remark from 'remark'
-import recommended from 'remark-preset-lint-recommended'
-import html from 'remark-html'
-import { useAsyncEffect } from '@react-hook/async'
+import remark from 'remark';
+import recommended from 'remark-preset-lint-recommended';
+import html from 'remark-html';
+import { useAsyncEffect } from '@react-hook/async';
+
+import { ReactCodeJar } from 'react-codejar';
 
 const MODE = {
   EDIT: 'EDIT',
   VIEW: 'VIEW',
-}
+};
 
 const MODE_BTN = [
   { value: MODE.VIEW, label: 'View' },
   { value: MODE.EDIT, label: 'Edit' },
-]
+];
 
 const compileMd = async md => {
   if (!md) {
-    return <></>
+    return <></>;
   }
-  const data = await remark().use(recommended).use(html).process(md)
+  const data = await remark().use(recommended).use(html).process(md);
   return (
     <div
       dangerouslySetInnerHTML={{
         __html: data.contents,
       }}
     ></div>
-  )
-}
+  );
+};
+
+const highlight = editor => {
+  let code = editor.textContent;
+  code = code.replace(/\((\w+?)(\b)/g, '(<font color="#8a2be2">$1</font>$2');
+  editor.innerHTML = code;
+};
 
 const ContentEditor = ({ className }) => {
-  const editorEl = useRef(null)
-  const [mode, setMode] = useState(MODE.EDIT)
-  const [markdownText, setMarkdownText] = useState('')
+  const [mode, setMode] = useState(MODE.EDIT);
+  const [markdownText, setMarkdownText] = useState('');
   const { status, value } = useAsyncEffect(() => {
     if (mode === MODE.EDIT) {
-      return Promise.resolve(<></>)
+      return Promise.resolve(<></>);
     }
-    return compileMd(markdownText)
-  }, [mode])
+    return compileMd(markdownText);
+  }, [mode]);
 
-  const isEdit = mode === MODE.EDIT
+  const isEdit = mode === MODE.EDIT;
   return (
     <Card className={classNames(ContentEditor.name, className)} data-component-name={ContentEditor.name}>
       <Card.Header>
@@ -67,22 +73,20 @@ const ContentEditor = ({ className }) => {
       </Card.Header>
       <Card.Body>
         {isEdit && (
-          <FormControl
-            as="textarea"
-            rows={20}
-            ref={editorEl}
-            value={markdownText}
-            onChange={e => setMarkdownText(e.currentTarget.value)}
-          ></FormControl>
+          <ReactCodeJar
+            code={setMarkdownText} // Initial code value
+            onUpdate={setMarkdownText} // Update the text
+            highlight={highlight} // Highlight function, receive the editor
+          />
         )}
         {!isEdit && status === 'success' && <div>{value}</div>}
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
 ContentEditor.propTypes = {
   className: PropTypes.string,
-}
+};
 
-export default ContentEditor
+export default ContentEditor;
